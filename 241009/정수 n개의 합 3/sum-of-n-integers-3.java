6aa1b9b14eb92544732bff4ea2924e4ed3fc7d17
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-09 19:58:07 +0900

    Title: 정수 n개의 합 3, Time: 286ms, Memory: 21MB, Status: Passed - CodeTree

--- 241009/정수 n개의 합 3/sum-of-n-integers-3.java
+++ 241009/정수 n개의 합 3/sum-of-n-integers-3.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
     private static int n,k;
     private static int[][] arr;
@@ -32,18 +31,17 @@ public class Main {
         prefix = new int[n+1][n+1];
         for(int i = 1; i<=n; i++) {
             for(int j = 1; j<=n; j++) {
-                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] -prefix[i-1][j-1] + arr[i][j];
+                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i][j];
             }
         }
-
-
     }
 
     private static void simulate() {
         int result = Integer.MIN_VALUE;
-        for(int i = 1; i<=n - k +1; i++) {
-            for(int j = 1; j<=n - k +1; j++) {
-                result = Math.max(result,prefix[i][j]-prefix[i-1][j]-prefix[i][j-1]-prefix[i-1][j-1]);
+        for(int i = k; i <= n; i++) {
+            for(int j = k; j <= n; j++) {
+                int sum = prefix[i][j] - prefix[i-k][j] - prefix[i][j-k] + prefix[i-k][j-k];
+                result = Math.max(result, sum);
             }
         }
 
