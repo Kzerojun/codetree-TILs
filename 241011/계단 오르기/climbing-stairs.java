fbc9396f0e47516e5b0471a0b36fc602b68b22d6
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-11 11:49:08 +0900

    Title: 계단 오르기, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241011/계단 오르기/climbing-stairs.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        init();
+        simulate();
+    }
+
+    private static void init() throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[1001];
+        for(int i = 0; i<=1000; i++) {
+            dp[i] = -1;
+        }
+    }
+
+    private static void simulate() {
+        int result = up(0);
+        System.out.println(result);
+    }
+
+    private static int up(int number) {
+        if(number>n) {
+            return 0;
+        }
+
+        if(number == n) {
+            return 1;
+        }
+
+        if(dp[number]!= -1) {
+            return dp[number];
+        }
+
+
+        int two = up(number+2);
+        int three = up(number+3);
+
+        return dp[number] = two+three;
+    }
+}
