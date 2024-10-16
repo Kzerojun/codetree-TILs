c2af308a88672b8ad2a58490da4a4eb9b1f22a47
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-16 20:07:06 +0900

    Title: 2차원 최대 증가 수열, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241016/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n,m;
+    static int[][] graph;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        init();
+        simulate();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        
+        graph = new int[n][m];
+        dp = new int[n][m];
+
+        for(int i = 0 ; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j= 0; j<m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+    }
+
+    private static void simulate() {
+        dfs(0,0);
+        System.out.println(dp[0][0]);
+    }
+
+    private static int dfs(int i, int j) {
+        if(dp[i][j] !=-1) {
+            return dp[i][j];
+        }
+
+        dp[i][j] = 1;
+
+        for(int x = i; x<n; x++) {
+            for(int y = j; y<m; y++) {
+                if(graph[i][j]<graph[x][y]) {
+                    int result = dfs(x,y);
+                    dp[i][j] = Math.max(dp[i][j],result+1);
+                }
+            }
+        }
+
+        return dp[i][j];
+    }
+}
