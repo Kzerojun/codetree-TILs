f4fb122c1b0fbe78ea6343b0cd84c3e632ce3885
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-16 17:36:56 +0900

    Title: 최대 점프 횟수, Time: 104ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241016/최대 점프 횟수/maximum-number-of-jumps.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+    static int[] dp;
+
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        init();
+        simulate();
+    }
+
+    private static void init() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = -1;
+        }
+    }
+
+    private static void simulate() {
+        for(int i = 0; i<n; i++) {
+            if(dp[i]==-1) {
+                dfs(i);
+            }
+        }
+
+        int result = -1;
+        for(int i =0; i<n; i++) {
+            result = Math.max(result,dp[i]);
+        }
+
+        System.out.println(result);
+    }
+
+    private static int dfs(int now) {
+        if(dp[now]!=-1) {
+            return dp[now];
+        }
+        
+        dp[now] =1;
+
+        for(int next = now; next<arr[now]; next++) {
+            int result = dfs(now);
+            dp[now] = Math.max(result+1,dp[now]);
+        }
+
+        return dp[now];
+    }
+
+}
