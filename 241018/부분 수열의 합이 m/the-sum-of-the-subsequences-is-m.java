9bfa522cdf7dd0ae0a09ef4adb4f017aefc018fb
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-18 21:00:54 +0900

    Title: 부분 수열의 합이 m, Time: 104ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 241018/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
+++ 241018/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
@@ -4,12 +4,11 @@ import java.io.*;
 public class Main {
     static int N, M;
     static int[] arr;
-    static int[] dp;
+    static int[][] dp;
     static final int INF = 10_001;
 
     public static void main(String[] args) throws IOException {
         init();
-        simulate();
     }
     
     private static void init() throws IOException {
@@ -20,43 +19,45 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         arr = new int[N];
-        dp = new int[M + 1];
+        dp = new int[101][10_001];
         
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.fill(dp, -1);
-    }
-
-    private static void simulate() {
-       for(int i =0  ;i<N; i++) {
-        dfs(M,i);
-       }
-       int result = dp[M];
-       System.out.println(result == INF ? -1 : result);
-    }
-
-    private static int dfs(int target, int index) {
-        if (target == 0) {
-            return 0;  // 수열의 길이를 반환하므로 0으로 변경
+        for(int i = 0; i<=100; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        if (target < 0 || index >= N) {
+
+        System.out.println(dfs(0,M));
+        
+    }
+
+
+    private static int dfs(int index, int target) {
+        if(target == 0) {
+            return 0;
+        }
+
+        if(target<0) {
             return INF;
         }
 
-        if (dp[target] != -1) {
-            return dp[target];
+        if(dp[index][target]!=-1) {
+            return dp[index][target];
         }
 
-        dp[target] = INF;
-        for (int i = index; i < N; i++) {
-            int result = dfs(target - arr[i], i + 1);
-            dp[target] = Math.min(result + 1, dp[target]);
+        dp[index][target] = INF;
+        for(int i = index; i<N; i++) {
+            if(target - arr[i] >=0) {
+                int result = dfs(i+1,target-arr[i]);
+                dp[index][target] = Math.min(dp[index][target],result+1);
+            }
         }
 
-        return dp[target];
+        return dp[index][target];
     }
+   
 }
