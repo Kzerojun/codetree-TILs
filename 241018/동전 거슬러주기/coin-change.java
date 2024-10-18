ce6935c5b1326eaebd3361070b3dc920f961a32f
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-18 19:36:47 +0900

    Title: 동전 거슬러주기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 241018/동전 거슬러주기/coin-change.java
+++ 241018/동전 거슬러주기/coin-change.java
@@ -2,72 +2,43 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N,M;
+    static int N, M;
     static int[] dp;
     static int[] coins;
+    static final int MAX_VALUE = 10001;
 
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
-        init();
-        simulate();
-    }
-
-    private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        dp = new int[M];   
+        dp = new int[M + 1];
         coins = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             coins[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i =0 ; i<M; i++) {
-            dp[i] = 10_000_001;
-        }
+        Arrays.fill(dp, -1);
+
+        int result = dfs(M);
+        System.out.println(result >= MAX_VALUE ? -1 : result);
     }
 
-    private static void simulate() {
-        for(int i = 0; i<N; i++) {
-            dfs(i);
+    private static int dfs(int amount) {
+        if (amount == 0) return 0;
+        if (amount < 0) return MAX_VALUE;
+        if (dp[amount] != -1) return dp[amount];
+
+        dp[amount] = MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            int result = dfs(amount - coins[i]);
+            dp[amount] = Math.min(dp[amount], result + 1);
         }
 
-        int result = Integer.MAX_VALUE;
-
-        for(int i =0  ;i<N; i++) {
-            result = Math.min(result,dp[i]);
-        }
-
-        System.out.println(result);
-    }
-
-    private static int dfs(int sum) {
-        if(sum == M) {
-            return 0;
-        }
-
-        if(sum> M) {
-            return 10_000_001;
-        }
-
-        if(dp[sum]!=10_000_001) {
-            return dp[sum];
-        }
-
-        dp[sum] = 10_000_001;
-
-        for(int i = 0 ; i<N;i++) {
-            if(sum<=M) {
-                int result = dfs(sum+coins[i]);
-                dp[sum] = Math.min(result+1,dp[sum]);
-            }
-        }
-
-        return dp[sum];
+        return dp[amount];
     }
 }
