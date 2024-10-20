ebbea30655e00f47c2c16b0895226c6b411f2bc8
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-20 20:16:41 +0900

    Title: 검색어와 접두사, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241020/검색어와 접두사/search-terms-and-prefixes.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N,M;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+
+        simulate();
+    }
+
+    private static void simulate() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        Trie trie = new Trie();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<N; i++) {
+            trie.insert(st.nextToken());
+        }
+
+        trie.sol(br.readLine());
+        
+    }
+}
+
+class Trie{
+    private final TrieNode root;
+
+    Trie() {
+        this.root = new TrieNode();
+    }
+
+    public void insert(String str) {
+        TrieNode current = root;
+
+        for(char ch : str.toCharArray()) {
+            current.child.putIfAbsent(ch, new TrieNode());
+            current = current.child.get(ch);
+            current.num++;
+        }
+
+        current.isTerminal = true;
+    }
+
+    public void sol(String str) {
+        TrieNode current = root;
+
+        for(char ch : str.toCharArray()) {
+            if(!current.child.containsKey(ch)) {
+                System.out.print(0+" ");
+            } else {
+                current = current.child.get(ch);
+                System.out.print(current.num+" ");
+            }
+        }
+        
+    }
+    
+}
+
+class TrieNode {
+    boolean isTerminal;
+    int num;
+    Map<Character,TrieNode> child;
+
+    TrieNode() {
+        this.isTerminal = false;
+        this.child = new TreeMap<>();
+        this.num = 0;
+    }
+}
