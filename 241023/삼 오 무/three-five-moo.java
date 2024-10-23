3f98e4b1a871c4d2ea3c8b04c3f2648ea01da87f
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-23 15:34:20 +0900

    Title: 삼 오 무, Time: 97ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241023/삼 오 무/three-five-moo.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int MAX_SIZE = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        simulate();
+    }
+
+    private static void simulate() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+
+        int left = 1;
+        int right = 16;
+
+        while(left<=right) {
+            int mid = (left+right)/2;
+
+            int result = cal(mid);
+            if(result>=N) {
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+            
+        }
+
+        System.out.println(left);
+    
+    }
+
+    private static int cal(int number) {
+        int sum = 0;
+
+        sum = sum + number/3;
+        sum = sum + number/5;
+        sum = sum - number/15;
+
+        // 내가 몇번째 숫자인지
+        return number-sum;
+    }
+}
