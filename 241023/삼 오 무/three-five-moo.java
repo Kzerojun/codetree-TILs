cfd571184e735af30881a36a868c84d24163c09b
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-23 15:35:48 +0900

    Title: 삼 오 무, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241023/삼 오 무/three-five-moo.java
+++ 241023/삼 오 무/three-five-moo.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int MAX_SIZE = 1_000_000_000;
+    static int MAX_SIZE = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
