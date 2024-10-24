544f8adfaa15aaa641420fbccafbd38bd2613b1a
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-10-24 17:49:44 +0900

    Title: 격자 칠하기 2, Time: 261ms, Memory: 18MB, Status: Passed - CodeTree

--- 241024/격자 칠하기 2/painting-the-grid-2.java
+++ 241024/격자 칠하기 2/painting-the-grid-2.java
@@ -2,87 +2,96 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N;
-    static int[][] graph;
-    static int[] dx ={-1,1,0,0};
-    static int[] dy ={0,0,1,-1};
-    static int goal;
+   static int N;
+   static int[][] graph;
+   static int[] dx = {-1, 1, 0, 0};
+   static int[] dy = {0, 0, 1, -1};
 
+   public static void main(String[] args) throws IOException {
+       simulate();
+   }
 
-    public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
-        simulate();
-    }
+   private static void simulate() throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    private static void simulate() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       N = Integer.parseInt(br.readLine());
+       graph = new int[N][N];
 
+       for(int i = 0; i < N; i++) {
+           StringTokenizer st = new StringTokenizer(br.readLine());
+           for(int j = 0; j < N; j++) {
+               graph[i][j] = Integer.parseInt(st.nextToken());
+           }
+       }
 
-        N = Integer.parseInt(br.readLine());
+       int left = 0;
+       int right = 1_000_000;
 
-        graph = new int[N][N];
-        
-        if(N*N%2==1){
-            goal = N*N/2+1;
-        }else {
-            goal = N*N/2;
-        }
+       while(left <= right) {
+           int mid = (left + right) / 2;
 
-        for(int i = 0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j =0 ;j<N; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+           if(isPossible(mid)) {
+               right = mid -1;
+           } else {
+               left = mid + 1;
+           }
+       }
 
+       System.out.println(left);
+   }
 
-        int left = 0;
-        int right = 1_000_000;
+   private static boolean isPossible(int mid) {
+       boolean[][] visited = new boolean[N][N];
 
+       for(int i = 0; i < N; i++) {
+           for(int j = 0; j < N; j++) {
+               if(!visited[i][j]) {
+                    int result = move(i, j, visited,mid);
+                   if(result>= N*N/2) {
+                        return true;
+                   }
+               }
+           }
+       }
+       return false;
+   }
 
-        while(left<=right) {
-            int mid = (left+right)/2;
+   private static int move(int x, int y, boolean[][] visited,int mid) {
+       Queue<Location> q = new LinkedList<>();
+       q.add(new Location(x, y));
+       visited[x][y] = true;
 
-            if(isPossible(mid)) {
-                right = mid-1;
-            }else {
-                left = mid +1;
-            }
-        }
+       int moveCount = 0;
 
-        System.out.println(left);
-        
-    }
+       while(!q.isEmpty()) {
+           Location now = q.poll();
+           moveCount++;
 
-    private static boolean isPossible(int mid) {
-        int cnt = 0;
+           for(int i = 0; i < 4; i++) {
+               int nx = dx[i] + now.x;
+               int ny = dy[i] + now.y;
 
-        for(int i =0  ; i<N; i++) {
-            for(int j =0 ; j<N; j++) {
-                if(graph[i][j] <= mid) {
-                    cnt++;
+               if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
+
+                if(Math.abs(graph[now.x][now.y] - graph[nx][ny])<= mid) {
+                    visited[nx][ny] = true;
+                     q.add(new Location(nx, ny));
                 }
-            }
-        }
 
 
-        if(cnt >=goal) {
-            return true;
-        }
+           }
+       }
 
-        return false;
-    }
-
-  
+       return moveCount;
+   }
 }
 
-
 class Location {
-    int x;
-    int y;
+   int x;
+   int y;
 
-    Location(int x, int y) {
-        this.x = x;
-        this.y =y;
-    }
+   Location(int x, int y) {
+       this.x = x;
+       this.y = y;
+   }
 }
