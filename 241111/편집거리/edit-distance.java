5740553a7886d1e4db0d563c5d48bbf38834c44c
KzeroJun <105963431+Kzerojun@users.noreply.github.com>
2024-11-11 14:14:45 +0900

    Title: 편집거리, Time: 181ms, Memory: 15MB, Status: Passed - CodeTree

--- 241111/편집거리/edit-distance.java
+++ 241111/편집거리/edit-distance.java
@@ -13,20 +13,19 @@ public class Main {
         A = br.readLine();
         B = br.readLine();
 
-        dp = new int[A.length()][B.length()];
+        dp = new int[A.length()+1][B.length()+1];
 
-        for(int i = 0 ; i<Math.max(A.length(),B.length()); i++) {
+        for(int i = 0 ; i<=A.length(); i++) {
             Arrays.fill(dp[i],-1);
         }
 
-        int result = dp(A.length()-1,B.length()-1);
-
+        int result = dp(A.length(),B.length());
         System.out.println(result);
     }
 
     private static int dp(int aIndex, int bIndex) {
-        if(aIndex < 0 ) return bIndex;
-        if(bIndex < 0) return aIndex;
+        if(aIndex == 0 ) return bIndex;
+        if(bIndex == 0) return aIndex;
 
 
         if(dp[aIndex][bIndex] !=-1) {
@@ -35,19 +34,20 @@ public class Main {
 
         dp[aIndex][bIndex] = 1_000_001;
 
-        char aCh = A.charAt(aIndex);
-        char bCh = B.charAt(bIndex);
+        char aCh = A.charAt(aIndex-1);
+        char bCh = B.charAt(bIndex-1);
 
         if(aCh == bCh) {
-           int result = dp(aIndex,bIndex);
+           int result = dp(aIndex-1,bIndex-1);
            dp[aIndex][bIndex] = Math.min(result,dp[aIndex][bIndex]);
         }else {
             int exchange = dp(aIndex-1,bIndex-1)+1;
             int delete = dp(aIndex-1,bIndex)+1;
             int insert = dp(aIndex,bIndex-1)+1;
-
             dp[aIndex][bIndex] = Math.min(dp[aIndex][bIndex],Math.min(exchange,Math.min(delete,insert)));
         }
+
+
 
         return dp[aIndex][bIndex];
     }
